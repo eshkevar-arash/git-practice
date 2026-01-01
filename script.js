83e89388a935843ba050fb4982f47fc606e2e11d
Arash <eshkevar.arash1387@gmail.com>
2026-01-01 05:05:14 +0330

    fixed

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
-const isLogin = () => {
-    return true
-}
+
 function getMe(){
     return true
 }
 function register(){
     return true
-=======
+}
 const isLogin = (username) => {
     if(username){
         return true
     }else{
         return false
     }
->>>>>>> fixedIsloginFeature
+
 }
