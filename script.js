7a94b36d7b1b22c8b74f95ac6dd04edafa29c01b
Arash <eshkevar.arash1387@gmail.com>
2026-01-02 02:56:46 +0330

    new code to master

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 
 
 const isLogin = (username) => {
-    if(username === 'arash' || username === 'adel'){
-        return true
-    }else{
-        return false
-    }
-
+    return true
 }
 
+const newCode = 'new code'
